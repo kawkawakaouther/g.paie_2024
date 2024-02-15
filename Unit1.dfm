612e566ecedcf5c24bc047fac937328afc2c4a24
magenta.boudjait@gmail.com <kawter>
2024-02-15 22:04:41 +0100

    edition d'edit et button hello

--- Unit1.dfm
+++ Unit1.dfm
@@ -1,7 +1,7 @@
-object Form1: TForm1
+object frmMain: TfrmMain
   Left = 0
   Top = 0
-  Caption = 'Form1'
+  Caption = 'frmMain'
   ClientHeight = 201
   ClientWidth = 447
   Color = clBtnFace
@@ -13,4 +13,63 @@ object Form1: TForm1
   OldCreateOrder = False
   PixelsPerInch = 96
   TextHeight = 13
+  object dxLayoutControl1: TdxLayoutControl
+    Left = 0
+    Top = 0
+    Width = 447
+    Height = 201
+    Align = alClient
+    TabOrder = 0
+    ExplicitLeft = 240
+    ExplicitTop = 40
+    ExplicitWidth = 300
+    ExplicitHeight = 250
+    object cxButton1: TcxButton
+      Left = 186
+      Top = 88
+      Width = 75
+      Height = 25
+      Caption = 'Ok'
+      TabOrder = 1
+      OnClick = cxButton1Click
+    end
+    object Edit1: TEdit
+      Left = 12
+      Top = 12
+      Width = 423
+      Height = 17
+      BevelInner = bvNone
+      BevelKind = bkFlat
+      BevelOuter = bvSpace
+      BorderStyle = bsNone
+      TabOrder = 0
+      Text = 'Edit1'
+    end
+    object dxLayoutControl1Group_Root: TdxLayoutGroup
+      AlignHorz = ahClient
+      AlignVert = avClient
+      ButtonOptions.Buttons = <>
+      Hidden = True
+      ShowBorder = False
+      Index = -1
+    end
+    object dxLayoutItem2: TdxLayoutItem
+      Parent = dxLayoutControl1Group_Root
+      AlignHorz = ahCenter
+      AlignVert = avCenter
+      CaptionOptions.Visible = False
+      Control = cxButton1
+      ControlOptions.OriginalHeight = 25
+      ControlOptions.OriginalWidth = 75
+      ControlOptions.ShowBorder = False
+      Index = 1
+    end
+    object dxLayoutItem1: TdxLayoutItem
+      Parent = dxLayoutControl1Group_Root
+      Control = Edit1
+      ControlOptions.OriginalHeight = 17
+      ControlOptions.OriginalWidth = 121
+      Index = 0
+    end
+  end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,35 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, cxGraphics, cxControls, cxLookAndFeels,
+  cxLookAndFeelPainters, dxSkinsCore, dxSkinBlack, dxSkinBlue, dxSkinBlueprint,
+  dxSkinCaramel, dxSkinCoffee, dxSkinDarkroom, dxSkinDarkSide,
+  dxSkinDevExpressDarkStyle, dxSkinDevExpressStyle, dxSkinFoggy,
+  dxSkinGlassOceans, dxSkinHighContrast, dxSkinLilian, dxSkinLiquidSky,
+  dxSkinLondonLiquidSky, dxSkinMcSkin, dxSkinMetropolis, dxSkinMetropolisDark,
+  dxSkinMoneyTwins, dxSkinOffice2007Black, dxSkinOffice2007Blue,
+  dxSkinOffice2007Green, dxSkinOffice2007Pink, dxSkinOffice2007Silver,
+  dxSkinOffice2010Black, dxSkinOffice2010Blue, dxSkinOffice2010Silver,
+  dxSkinOffice2013DarkGray, dxSkinOffice2013LightGray, dxSkinOffice2013White,
+  dxSkinOffice2016Colorful, dxSkinOffice2016Dark, dxSkinOffice2019Colorful,
+  dxSkinPumpkin, dxSkinSeven, dxSkinSevenClassic, dxSkinSharp, dxSkinSharpPlus,
+  dxSkinSilver, dxSkinSpringtime, dxSkinStardust, dxSkinSummer2008,
+  dxSkinTheAsphaltWorld, dxSkinTheBezier, dxSkinsDefaultPainters,
+  dxSkinValentine, dxSkinVisualStudio2013Blue, dxSkinVisualStudio2013Dark,
+  dxSkinVisualStudio2013Light, dxSkinVS2010, dxSkinWhiteprint,
+  dxSkinXmas2008Blue, cxContainer, cxEdit, dxLayoutcxEditAdapters,
+  dxLayoutControlAdapters, Vcl.Menus, Vcl.StdCtrls, cxButtons,
+  dxLayoutContainer, cxTextEdit, cxDBEdit, cxClasses, dxLayoutControl;
 
 type
-  TForm1 = class(TForm)
+  TfrmMain = class(TForm)
+    dxLayoutControl1Group_Root: TdxLayoutGroup;
+    dxLayoutControl1: TdxLayoutControl;
+    cxButton1: TcxButton;
+    dxLayoutItem2: TdxLayoutItem;
+    Edit1: TEdit;
+    dxLayoutItem1: TdxLayoutItem;
+    procedure cxButton1Click(Sender: TObject);
   private
     { Déclarations privées }
   public
@@ -15,10 +40,17 @@ type
   end;
 
 var
-  Form1: TForm1;
+  frmMain: TfrmMain;
 
 implementation
 
 {$R *.dfm}
+
+procedure TfrmMain.cxButton1Click(Sender: TObject);
+begin
+frmMain.Edit1.Text:='Hello';
+
+
+end;
 
 end.
